54bee2d3000c92e85f7478a88c8a2cbf445838d0
Ethylburner <79605559+Ethylburner@users.noreply.github.com>
2025-10-11 19:00:26 -0400

    Update Wristband.py
    added better access methods

--- Base-Station-Code/Wristband.py
+++ Base-Station-Code/Wristband.py
@@ -13,7 +13,8 @@ class Packet:
         self.rms = rms
         self.checkSum = (ver + seq + timestamp + flags + heartrate + spo2 + skinTemp + rms) % 256
         self.full = (self.checkSum<<(14*8)) | (rms<<(12*8)) | (skinTemp<<(10*8)) | (spo2<<(9*8)) | (heartrate<<(8*8)) | (flags<<(7*8)) | (timestamp<<(3*8)) | (seq<<(1*8)) | ver
-       
+    def __str__(self):
+        return hex(self.full)
 class Wristband:
     '''
     Class for wristbands, containing its name, id, and info packet.
@@ -22,7 +23,8 @@ class Wristband:
     def __init__(self, name):
         self.name = name
         self.packet = Packet(0x01, 0x003C, 0x0001D4C0, 0x02, 0x4C, 0x62, 0x0E42, 0x00B4)
-        
+    def getPacket(self):
+        return self.packet
 
 w1 = Wristband('Wristband1')
-print(hex(w1.packet.full))
+print(w1.getPacket())
