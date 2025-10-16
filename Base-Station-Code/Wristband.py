d55deeea7764ef215bb7733bdc7b36bdf9798653
Ethylburner <79605559+Ethylburner@users.noreply.github.com>
2025-10-16 17:18:45 -0400

    updated manager class to better reflect kevins notes

--- Base-Station-Code/Wristband.py
+++ Base-Station-Code/Wristband.py
@@ -1,30 +1,35 @@
 import pprint as pp
 import simplepyble
 import time
-class Packet:
-    def __init__(self, ver, seq, timestamp, flags, heartrate, spo2, skinTemp, rms):
-        self.ver = ver
-        self.seq = seq
-        self.timestamp = timestamp
-        self.flags = flags
-        self.heartrate = heartrate
-        self.spo2 = spo2
-        self.skinTemp = skinTemp
-        self.rms = rms
-        self.checkSum = (ver + seq + timestamp + flags + heartrate + spo2 + skinTemp + rms) % 256
-        self.full = (self.checkSum<<(14*8)) | (rms<<(12*8)) | (skinTemp<<(10*8)) | (spo2<<(9*8)) | (heartrate<<(8*8)) | (flags<<(7*8)) | (timestamp<<(3*8)) | (seq<<(1*8)) | ver
-    def __str__(self):
-        return hex(self.full)
+# class Packet:
+#     def __init__(packet):
+#         self.ver = ver
+#         self.seq = seq
+#         self.timestamp = timestamp
+#         self.flags = flags
+#         self.heartrate = heartrate
+#         self.spo2 = spo2
+#         self.skinTemp = skinTemp
+#         self.rms = rms
+#         self.checkSum = (ver + seq + timestamp + flags + heartrate + spo2 + skinTemp + rms) % 256
+#         self.full = (self.checkSum<<(14*8)) | (rms<<(12*8)) | (skinTemp<<(10*8)) | (spo2<<(9*8)) | (heartrate<<(8*8)) | (flags<<(7*8)) | (timestamp<<(3*8)) | (seq<<(1*8)) | ver
+#     def __str__(self):
+#         return hex(self.full)
 class Wristband:
     '''
     Class for wristbands, containing its name, id, and info packet.
     '''
-    wristId = 0
-    def __init__(self, name):
+    def __init__(self, name, id):
         self.name = name
-        self.packet = Packet(0x01, 0x003C, 0x0001D4C0, 0x02, 0x4C, 0x62, 0x0E42, 0x00B4)
-    def getPacket(self):
-        return self.packet
+        self.id = id
+        self.packet = 0x0
+    def receive_packet(data):
+        incomingPacket = data
+        if sum(incomingPacket[0:13]) != incomingPacket[14]:
+            return
+        else:
+            packet = data
 
-w1 = Wristband('Wristband1')
+
+w1 = Wristband('Wristband1', 0)
 print(w1.getPacket())
--- Base-Station-Code/bluetooth_impl1.py
+++ Base-Station-Code/bluetooth_impl1.py
@@ -8,12 +8,18 @@
 import pprint as pp
 import simplepyble
 import time
+import Wristband
 
-bracletNames = "VTL-1"
+BraceletNames = "VTL-1"
+ServiceUUID = 0xa1a3b2d8e13f5f9b4b4aa822f1845a8f
+CharUUID = 0x99113f5e2a7d5a8d2a4bb239b7c4e2d3
+Wristband1 = Wristband("Wristband1", 0)
+Wristbands = [Wristband1]
 class BraceletManager:
 	"""
 	This class contains the
 	"""
+	
 	def __init__(self):
 		self.adapter = simplepyble.Adapter.get_adapters()
 
@@ -25,7 +31,7 @@ class BraceletManager:
 
 	def assign_id(self, peripheral) -> int:
 		"""
-		Assigns a new ID to a braclet if not already assigned.
+		Assigns a new ID to a Bracelet if not already assigned.
 
 		:param peripheral:
 		:return:
@@ -40,33 +46,33 @@ class BraceletManager:
 		return self.assigned_devices[addr]
 
 	def scan_and_connect(self):
-        """
-        """
-        print("Scanning for bracelets......")
-        # scan for around 10 seconds
-        self.adapter.scan_for(10000)
-        peripherals = self.adapter.scan_get_results()
+		print("Scanning for bracelets......")
+		# scan for around 10 seconds
+		self.adapter.scan_for(10000)
+		peripherals = self.adapter.scan_get_results()
 
-        for p in peripherals:
+		for p in peripherals:
             # Look for devices with the name convention we will use
-            if bracletNames in p.idenitfiers():
-                #
-                braclet_id = self.assign_id(p)
-                print(f"Braclet #{braclet_id} deteced: {p.idenitfier()}")
+			if BraceletNames in p.idenitfiers():
+				Bracelet_id = self.assign_id(p)
+				
+				print(f"Bracelet #{Bracelet_id} deteced: {p.idenitfier()}")
+				try:
+					p.connect()
+					print(f"Bracelet #{Bracelet_id} connected.")
+					p.notify(ServiceUUID, CharUUID, Wristbands[Bracelet_id].receive_packet)
+					p.disconnect()
+					print(f"Bracelet #{Bracelet_id} disconnected (test complete.)")
+				except Exception as e:
+					print(f"Bracelet #{Bracelet_id} failed to connect.")
+				
+	def run(self):
+		while True:
+			self.scan_and_connect()
+			# wait before the next scan cycle
+			time.sleep(8)
+	
 
-                try:
-                    p.connect()
-                    print(f"Braclet #{braclet_id} connected.")
-                    p.disconnect()
-                    print(f"Braclet #{braclet_id} disconnected (test complete.")
-                except Exception as e:
-                    print(f"Braclet #{braclet_id} failed to connect.")
-
-   def run(self):
-	while True:
-		self.scan_and_connect()
-		# wait before the next scan cycle
-		time.sleep(8)
 
 if __name__ == "__main__":
 	manager = BraceletManager()
